3138367d81e505e457679a0f8123af3760edfbda
Syed Danish Ahmed <daniahmed758@gmail.com>
2025-05-13 07:38:43 -0700

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-# def get_temperatures():
-#     temperatures = []
-#     print("Enter temperatures for 7 days:")
-#     for i in range(7):
-#         temp = float(input(f"Day {i+1} temperature (°C): "))
-#         temperatures.append(temp)
-#     return temperatures
-
-# def calculate_average(temps):
-#     return sum(temps) / len(temps)
-
-# def calculate_max(temps):
-#     return max(temps)
-
-# def calculate_min(temps):
-#     return min(temps)
-
-# def display_results(temps):
-#     avg = calculate_average(temps)
-#     highest = calculate_max(temps)
-#     lowest = calculate_min(temps)
-
-#     print("\n--- Weekly Temperature Summary ---")
-#     # for i, temp in enumerate(temps):
-#     #     print(f"Day {i+1}: {temp}°C")
-#     print(f"\nAverage Temperature: {avg:.2f}°C")
-#     print(f"Highest Temperature: {highest}°C")
-#     print(f"Lowest Temperature: {lowest}°C")
-# temperatures = get_temperatures()
-# display_results(temperatures)
-
 def get_temperatures():
     temperatures=[]
     print("Enter temperatures for 7 days")
